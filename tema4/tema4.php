acd96aaa97201e8b49a59c6ed93bef4adb15b026
Ecaterina <barganecaterina@yahoo.com>
2023-05-08 21:33:53 +0300

    tema4/tema4:

--- tema4/tema4.php
+++ tema4/tema4.php
@@ -1,9 +1,15 @@
 <?php 
 
-$i = 0;
-
-for (i=0; i<=30; i++;) {
-
+for ($i = 0; $i <= 29; $i++) {
+ echo $i . " + ";
 }
+echo 30;
+$sum = 0;
+
+for ($i = 0; $i <= 30; $i++) {
+$sum = $sum + $i;
+}
+
+echo " = " . $sum;
 
 ?>
