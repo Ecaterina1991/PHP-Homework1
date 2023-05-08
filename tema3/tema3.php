7e9bd87f11cb98b82b2def1c9586a6b5f1d42e51
Ecaterina <barganecaterina@yahoo.com>
2023-05-08 21:02:47 +0300

    tema: 3

---
+++ tema3/tema3.php
@@ -0,0 +1,19 @@
+<?php
+
+$numere = array(6, 4, 15, 12, 36, 89, 45);
+
+foreach ($numere as $i) {
+   if ($i % 6 == 0) {
+    echo $i . "<br>";
+   }
+   else {
+    echo "";
+   }
+}
+
+
+
+
+
+
+?>
